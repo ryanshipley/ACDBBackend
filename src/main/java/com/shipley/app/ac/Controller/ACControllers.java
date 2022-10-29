e72fbeb7bed226327dba7919b6a4f512b16eb50b
ryanshipley <ryanshipley23@gmail.com>
2022-10-29 14:07:55 -0400

    Creates Initial and save routes

--- src/main/java/com/shipley/app/ac/Controller/ACControllers.java
+++ src/main/java/com/shipley/app/ac/Controller/ACControllers.java
@@ -1,13 +1,33 @@
 package com.shipley.app.ac.Controller;
 
+import com.shipley.app.ac.Repo.Gamerepo;
+import com.shipley.app.ac.models.Game;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ACControllers {
 
+    @Autowired
+    private Gamerepo gameRepo;
+
     @GetMapping(value = "/")
     public String getPage(){
         return "Welcome";
+    }
+@GetMapping(value = "/games")
+    public List<Game> getGames(){
+        return gameRepo.findAll();
+    }
+
+    @PostMapping(value = "/save")
+    public String saveGame(@RequestBody Game game){
+        gameRepo.save(game);
+        return "Saved....";
     }
 }
