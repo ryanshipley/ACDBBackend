c67ccc06c4b55b6bb65debc46fa56697d17502eb
ryanshipley <ryanshipley23@gmail.com>
2022-10-30 14:03:25 -0400

    Adds history element

--- src/main/java/com/shipley/app/ac/Controller/ACControllers.java
+++ src/main/java/com/shipley/app/ac/Controller/ACControllers.java
@@ -3,12 +3,10 @@ package com.shipley.app.ac.Controller;
 import com.shipley.app.ac.Repo.Gamerepo;
 import com.shipley.app.ac.models.Game;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 public class ACControllers {
@@ -20,7 +18,7 @@ public class ACControllers {
     public String getPage(){
         return "Welcome";
     }
-@GetMapping(value = "/games")
+    @GetMapping(value = "/games")
     public List<Game> getGames(){
         return gameRepo.findAll();
     }
